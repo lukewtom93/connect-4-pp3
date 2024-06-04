8a5f9bd71c80cf3bb97607ff976310199c3ce0d0
Luke Tomlinson <lukewtom93@gmail.com>
2024-06-04 14:15:10 +0000

    Added functions to play a peice into the board

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
 def create_board():
     """
     Creates blank board
@@ -14,6 +17,13 @@ def valid_location(board, col):
     """
     return board[5][col] == 0
 
+def next_open_row(board, col):
+    for row in range(ROW_COUNT):
+        if board[row][col] == 0:
+            return row
+
+def drop_peice(board, row, col, peice):
+    board[row][col] = peice
 
 board = create_board()
 print(board)
@@ -23,8 +33,18 @@ turn = 0
 while not game_over:
     if turn == 0:
         col = int(input("Player One's up\nSelect a column between 0-6: "))
+
+        if valid_location(board, col):
+            row = next_open_row(board, col)
+            drop_peice(board, row, col, 1)
     else:
         col = int(input("Player Two's up\nSelect a column between 0-6: "))
+
+        if valid_location(board, col):
+            row = next_open_row(board, col)
+            drop_peice(board, row, col, 2)
+
+    print(board)
 
     turn += 1
     turn = turn % 2 
