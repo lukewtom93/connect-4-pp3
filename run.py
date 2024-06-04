2005cc0644c53136ee2e2e6b48a410c30d26d12f
Luke Tomlinson <lukewtom93@gmail.com>
2024-06-04 12:48:47 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import numpy
+
+def create_board():
+    board = numpy.zeros((6, 7))
+    return board
+
+board = create_board()
+print(board)
