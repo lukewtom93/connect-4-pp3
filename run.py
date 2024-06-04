104297db53484bcff1fdd1dcb7ec24b221d5103f
Luke Tomlinson <lukewtom93@gmail.com>
2024-06-04 13:08:18 +0000

    Added while loop to alternate between player one and two

--- run.py
+++ run.py
@@ -1,8 +1,23 @@
-import numpy
+import numpy as np
 
 def create_board():
-    board = numpy.zeros((6, 7))
+    """
+    Creates blank board
+    """
+    board = np.zeros((6, 7))
     return board
 
 board = create_board()
-print(board)
+game_over = False
+turn = 0
+
+while not game_over:
+    if turn == 0:
+        selection = int(input("Player One's up\nSelect a column between 1-6: "))
+    else:
+        selection = int(input("Player Two's up\nSelect a column between 1-6: "))
+
+    turn += 1
+    turn = turn % 2 
+
+    print(selection)
