6c4aec319c14e3472435e461896041ae8b71d6fa
Luke Tomlinson <lukewtom93@gmail.com>
2024-06-04 14:05:22 +0000

    Added valid_location function

--- run.py
+++ run.py
@@ -7,17 +7,26 @@ def create_board():
     board = np.zeros((6, 7))
     return board
 
+def valid_location(board, col):
+    """
+    Checking location on the board is free
+    to play into.
+    """
+    return board[5][col] == 0
+
+
 board = create_board()
+print(board)
 game_over = False
 turn = 0
 
 while not game_over:
     if turn == 0:
-        selection = int(input("Player One's up\nSelect a column between 1-6: "))
+        col = int(input("Player One's up\nSelect a column between 0-6: "))
     else:
-        selection = int(input("Player Two's up\nSelect a column between 1-6: "))
+        col = int(input("Player Two's up\nSelect a column between 0-6: "))
 
     turn += 1
     turn = turn % 2 
 
-    print(selection)
+    print(col)
