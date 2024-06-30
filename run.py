f5a344479403c256aa3bb340100a8ceffbc2a48d
Luke Tomlinson <lukewtom93@gmail.com>
2024-06-30 06:51:39 +0000

    Added winning move function that checks the horizontal positions

--- run.py
+++ run.py
@@ -23,7 +23,7 @@ def valid_location(board, col):
     Checking location on the board is free
     to play into.
     """
-    return board[ROW_COUNT -1][col] == 0
+    return board[ROW_COUNT-1][col] == 0
 
 def next_open_row(board, col):
     """
@@ -39,6 +39,17 @@ def drop_peice(board, row, col, peice):
     """
     board[row][col] = peice
 
+def winning_move(board, peice):
+    """
+    Checks for 4 peices in a row
+    """
+    #Horizontal row
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == peice and board[r][c+1] == peice and board[r][c+2] == peice and board[r][c+3]:
+                return True
+
+
 board = create_board()
 print(board)
 game_over = False
@@ -51,6 +62,10 @@ while not game_over:
         if valid_location(board, col):
             row = next_open_row(board, col)
             drop_peice(board, row, col, 1)
+
+            if winning_move(board, 1):
+                print("Player 1 wins")
+                game_over = True
     else:
         col = int(input("Player Two's up\nSelect a column between 0-6: \n"))
 
