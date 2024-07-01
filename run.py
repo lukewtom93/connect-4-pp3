7784dcca03ba13b427eb518c9f8edb60f365807f
Luke Tomlinson <lukewtom93@gmail.com>
2024-07-01 11:31:24 +0000

    Added run_game function and try except statment within the while loop

--- run.py
+++ run.py
@@ -22,10 +22,10 @@ def valid_location(board, col):
     """
     Checking location on the board is free
     to play into.
-    """
+    """  
     
-    if col <= ROW_COUNT:
-        return board[ROW_COUNT-1][col] == 0
+  
+    return board[ROW_COUNT-1][col] == 0
 
         
     
@@ -72,40 +72,57 @@ def winning_move(board, peice):
             if board[r][c] == peice and board[r-1][c+1] == peice and board[r-2][c+2] == peice and board[r-3][c+3]:
                 return True
 
+def run_game():
 
 
-board = create_board()
-print(board)
-game_over = False
-turn = 0
+    board = create_board()
+    print(board)
+    game_over = False
+    turn = 0
 
-while not game_over:
-    if turn == 0:
-        col = int(input("Player One's up\nSelect a column between 0-6: \n"))
+    while not game_over:
+        if turn == 0:
 
-        if valid_location(board, col):
-            row = next_open_row(board, col)
-            drop_peice(board, row, col, 1)
-        else:     
-            print('Invalid selection\nPlease select number between 0-6')
-            continue
-            if winning_move(board, 1):
-                print("Player 1 wins")
-                game_over = True
-    else:
-        col = int(input("Player Two's up\nSelect a column between 0-6: \n"))
+            try:
+                col = int(input("Player One's up\nSelect a column between 0-6: "))
+                if col > ROW_COUNT:
+                    raise ValueError(f'{col} is not a column.')
+                    
+            except ValueError as e:
+                print(f'Oops!\n{e}')
+                continue
 
-        if valid_location(board, col):
-            row = next_open_row(board, col)
-            drop_peice(board, row, col, 2)
+            if valid_location(board, col):
+                row = next_open_row(board, col)
+                drop_peice(board, row, col, 1)
 
-            if winning_move(board, 2):
-                print("Player 1 wins")
-                game_over = True
+                if winning_move(board, 1):
+                    print("Player 1 wins")
+                    game_over = True
+        else:
+            
+            try:
+                col = int(input("Player Two's up\nSelect a column between 0-6: "))
+                if col > ROW_COUNT:
+                    raise ValueError(f'{col} is not a column.')
+                    
+            except ValueError as e:
+                print(f'Oops!\n{e}')
+                continue
 
-    print_board(board)
+            if valid_location(board, col):
+                row = next_open_row(board, col)
+                drop_peice(board, row, col, 2)
 
-    turn += 1
-    turn = turn % 2 
+                if winning_move(board, 2):
+                    print("Player 2 wins")
+                    game_over = True
 
-    print(col)
+        print_board(board)
+
+        turn += 1
+        turn = turn % 2 
+
+        print(col)
+
+run_game()
